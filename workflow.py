f6394311db00216c449564cf5dba9195ffb76ea7
Cristina Seva <37145016+cocosev@users.noreply.github.com>
2018-06-18 16:01:46 +0200

    Update workflow.py

--- workflow.py
+++ workflow.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Jun 11 13:34:41 2018
-
-@author: booort, ruhugu
-"""
 
 from networkx import Graph, DiGraph, simple_cycles, find_cycle, cycle_basis
 import networkx as nx
@@ -19,39 +14,39 @@ mi ordenador no tira no con ella, por eso esta comentada
 
 # Load data
 # ========================================
-# Get the information from both files
+# Gets the information from both files
 dictionary1 = csv2dict("sevaseviene2network.csv", delimiter=";")
 #dictionary2 = csv2dict("Definitiva.csv", delimiter=";")
 
-# These function transform the dictionary to a directed graph networkx-style
+# This function transforms the dictionary to a directed graph networkx-style
 DG1 = DiGraph(dictionary1)
 #DG2 = DiGraph(dictionary2)
 
-# Remove nodes with out degree 0
+# Removes nodes with out degree 0
 remove = [node for node in DG1.nodes if DG1.out_degree(node) <= 0]
 DG1.remove_nodes_from(remove)
 
-# These function transform the dictionary to an undirected graph networkx-style
+# This function transforms the dictionary to an undirected graph networkx-style
 undirected1 = DG1.to_undirected()
 #undirected2 = DG2.to_undirected()
 
 
 # Count cycles
 # ==============================
-# These functions counts all the feedback cycles in our graph
+# This function counts all the feedback cycles in our graph
 feedback_cycle_list_DG1 = (list(simple_cycles(DG1)))
 #cycle_list_DG2 = (list(simple_cycles(DG2)))
 
 
-# Finally this part should gets all loops via networkx function
+# Finally this part should get all loops via networkx function
 cycle_list_DG1 = simple_cycles_undirected(undirected1, maxlength=10)
 
 
-# Save cycles to file
+# Saves cycles to file
 # ========================================
-# Save directed cycles
+# Saves directed cycles
 savecycles("sevaseviene_directedcycles.dat", feedback_cycle_list_DG1)
-# Save undirected cycles
+# Saves undirected cycles
 savecycles("sevaseviene_undirectedcycles.dat", cycle_list_DG1)
 
 
